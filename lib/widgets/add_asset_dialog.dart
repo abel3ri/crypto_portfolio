19e6ae57e40c22f78459c64322c5bbe2b249c0e5
abel3ri <abelmerete@yahoo.com>
2024-08-01 01:06:42 +0300

    add: data controller and consume the controller

--- lib/widgets/add_asset_dialog.dart
+++ lib/widgets/add_asset_dialog.dart
@@ -1,28 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_state_mgmt/services/http_service.dart';
+
+class AddAssetDialogController extends GetxController {
+  RxBool loading = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _getAssets();
+  }
+
+  Future<void> _getAssets() async {
+    loading.value = true;
+    HTTPService httpService = Get.find<HTTPService>();
+    dynamic data = await httpService.get(path: "currencies");
+    print(data);
+    loading.value = false;
+  }
+}
 
 class AddAssetDialog extends StatelessWidget {
-  const AddAssetDialog({super.key});
+  final controller = Get.put(
+    AddAssetDialogController(),
+  );
+
+  AddAssetDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Material(
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.4,
-          width: MediaQuery.of(context).size.width * 0.8,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: Colors.white,
+    return Obx(
+      () => Center(
+        child: Material(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white,
+            ),
+            child: _buildUI(),
           ),
-          child: _buildUI(),
         ),
       ),
     );
   }
 
   Widget _buildUI() {
+    if (controller.loading.isTrue) {
+      return Center(
+        child: SizedBox(
+          width: 30,
+          height: 30,
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
     return Column(
-      children: [],
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Data loaded"),
+      ],
     );
   }
 }
